73aa6c47dca218f95d83e1f94fda4d3b97946faf
lupasos <lupiandresm@gmail.com>
2025-10-01 21:26:36 -0500

    Entendi casi todo de este

--- Perro.cpp
+++ Perro.cpp
@@ -1,35 +1,44 @@
-//
-// Created by lufe and Gonzo.
-//
-
 #include "Perro.h"
+#include <iostream>
 using namespace std;
 
 void Perro::ladrar() {
-
-    cout << " Esta ladrando " << nombre << ", tengo " << edad << " anios " << std::endl;
+    cout << "Esta ladrando " << nombre << " tengo " << edad << " anios" << endl;
 }
 
 void Perro::saludar() {
-    int veces = 5; // veces es una variable local
+    int veces = 3;
     for (int i = 0; i < veces; i++) {
-       // Esta linea combina variables locales con atributos de instancia
-        cout << nombre << " saluda." << std::endl;
+        cout << nombre << " saluda." << endl;
     }
 }
 
-
 void Perro::alimentar() {
-    float cantidad = 0.0; //Pedazo sacado con la IA para poder entender correctamente lo que se esta haciendo, los kilos a dar
-    if (tamanio == "Grande") {
-        cantidad = 1.0 * edad;
+    float cantidad = 0.0f;
+    if (tamanio == "Grande" || tamanio == "grande") {
+        cantidad = 1.0f * edad;
+    } else if (tamanio == "Mediano" || tamanio == "mediano") {
+        cantidad = 0.5f * edad;
+    } else {
+        cantidad = 0.25f * edad;
     }
-    else if (tamanio == "Mediano") {
-        cantidad = 0.5 * edad;
-    }
-    else {
-        cantidad = 0.25 * edad;
-    }
+    cout << "Alimentando a " << nombre << ": " << cantidad << " kg." << endl;
+}
 
-    cout << "A " << nombre       << " se le esta dando " <<cantidad << " kgs de comida" << endl;
+void Perro::mostrarRaza() {
+    if (razaPtr != nullptr) {
+        cout << "Raza: " << razaPtr->getNombre()
+             << " (Pais: " << razaPtr->getPaisOrigen() << ")" << endl;
+    } else {
+        cout << nombre << " no tiene raza asignada." << endl;
+    }
+}
+
+void Perro::mostrarVeterinario() {
+    if (vetPtr != nullptr) {
+        cout << "Veterinario: " << vetPtr->getNombre()
+             << " (Experiencia: " << vetPtr->getAniosExperiencia() << " anios)" << endl;
+    } else {
+        cout << nombre << " no tiene veterinario asignado." << endl;
+    }
 }
